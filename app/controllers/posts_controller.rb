b6ce980f7311c835997c2764ae581923d94ce4b9
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-05-09 22:54:07 +0900

    一通り実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.order(created_at: :desc)
+    @new_posts = Post.order(created_at: :desc).limit(5)
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to "/posts"
+  end
+
+  private
+  def post_params #ストロングパラメータを定義する
+    params.require(:post).permit(:title, :body, :category)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'my_blog/index' => 'post#index'
-  get 'my_blog/keisan/:id1/tasu/:id2' => 'keisan#tasu'
-  get 'my_blog/keisan/:id1/hiku/:id2' => 'keisan#hiku'
-  get 'my_blog/keisan/:id1/:else/:id2' => 'keisan#else'
+#  get 'posts/index'
+#
+#  get 'posts/show'
+#
+#  get 'posts/new'
+#
+#  get 'posts/create'
+#
+#  get 'posts/edit'
+#
+#  get 'posts/update'
+#
+#  get 'posts/destroy'
+#
+#  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#  get 'my_blog/index' => 'post#index'
+#  get 'my_blog/keisan/:id1/tasu/:id2' => 'keisan#tasu'
+#  get 'my_blog/keisan/:id1/hiku/:id2' => 'keisan#hiku'
+#  get 'my_blog/keisan/:id1/:else/:id2' => 'keisan#else'
+  
+  resources :posts
+  root 'posts#index'
   
 end
